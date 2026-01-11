a98270d16ae7079b28d019416edbc07458283929
pritsdesai2008-lab <pritsdesai2008@gmail.com>
2026-01-11 09:16:15 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'anaes-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './tailwind.js',
+  './jspdf.js',
+  './autotable.js',
+  './qrcode.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
